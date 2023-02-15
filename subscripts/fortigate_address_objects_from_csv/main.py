236c99cedfe8f98baee1c380d56ee6636c5c54b1
root <root@LMXPS13.internal.lmccormick.com>
2023-02-15 13:39:22 -0600

    cleanup

--- subscripts/fortigate_address_objects_from_csv/main.py
+++ subscripts/fortigate_address_objects_from_csv/main.py
@@ -13,23 +13,23 @@ with open(input_file) as csvfile:
         if row['Type'] == "subnet":
             name = str(row['Name'])
             address = str(row['Network'])
-            current_string = "config firewall address\nedit " + name + "\nset subnet " + address + "\nnext\nend\n"
+            current_string = 'config firewall address\nedit "' + name + '"\nset subnet ' + address + "\nnext\nend\n"
             string += current_string
         if row['Type'] == "geography":
             name = str(row['Name'])
             country = str(row['country'])
-            current_string = "config firewall address\nedit " + name + "\nset type geography\nset country " + country + "\nnext\nend\n"
+            current_string = 'config firewall address\nedit "' + name + '"\nset type geography\nset country ' + country + "\nnext\nend\n"
             string += current_string
         if row['Type'] == "fqdn":
             name = str(row['Name'])
             value = str(row['FQDN'])
-            current_string = "config firewall address\nedit " + name + "\nset type fqdn\nset fqdn " + value + "\nnext\nend\n"
+            current_string = 'config firewall address\nedit "' + name + '"\nset type fqdn\nset fqdn ' + value + "\nnext\nend\n"
             string += current_string
         if row['Type'] == "iprange":
             name = str(row['Name'])
             value1 = str(row['Start-IP'])
             value2 = str(row['End-IP'])
-            current_string = "config firewall address\n edit " + name + "\nset type iprange\nset start-ip " + value1 + "\nset end-ip " + value2 + "\nnext\nend\n"
+            current_string = 'config firewall address\n edit "' + name + '"\nset type iprange\nset start-ip ' + value1 + "\nset end-ip " + value2 + "\nnext\nend\n"
             string += current_string
 
 
